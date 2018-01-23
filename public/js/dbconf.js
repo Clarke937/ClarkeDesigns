a75e484c636998ac1145e6c00ed44da222354977
edgar-pc <eretana60@gmail.com>
2018-01-23 09:20:13 -0600

    Version 1: Trabajada la parte de tutoriales

---
+++ public/js/dbconf.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyA5VkcRs8Rbqy8XkQT1h0Gw8RUIk9BMpYA",
+  authDomain: "clarke-designs.firebaseapp.com",
+  databaseURL: "https://clarke-designs.firebaseio.com",
+  projectId: "clarke-designs",
+  storageBucket: "clarke-designs.appspot.com",
+  messagingSenderId: "280397753835"
+};
+firebase.initializeApp(config);
